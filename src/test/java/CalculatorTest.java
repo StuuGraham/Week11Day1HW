895933a073aa924e37e6fdce1f60e07092a0c148
StuuGraham <stuartgraham7@hotmail.co.uk>
2021-01-05 18:17:26 +0000

    setup CalculatorTest.java

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void setUp() { calculator = new Calculator();}
+
+    @Test
+    public void canAdd(){
+        assertEquals(7, calculator.addNumbers());
+    }
+
+    @Test
+    public void canSubtract(){
+        assertEquals(3, calculator.subtractNumbers());
+    }
+
+    @Test
+    public void canMultiply(){
+        assertEquals(10, calculator.multiplyNumbers());
+    }
+
+    @Test
+    public void canDivide(){
+        asserEquals(2.5, calculator.divideNumbers());
+    }
+
+}
